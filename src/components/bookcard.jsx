a322b4b48a0644f7910ce12d119833cffd28a1cb
DIVEN009 <sainidiven@gmail.com>
2025-03-17 09:52:55 +0530

    added

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,26 @@
 // write the book component code here
+import PropTypes from 'prop-types';
+import '../components/bookcard.css';
+
+
+const BookCard = ({ book }) => {
+  return (
+    <div className="book-card">
+      <h2>{book.title}</h2>
+      <p><strong>Author:</strong> {book.author}</p>
+      <p><strong>Year:</strong> {book.year}</p>
+      <p><strong>Genre:</strong> {book.genre}</p>
+    </div>
+  );
+};
+
+BookCard.propTypes = {
+  book: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
+    genre: PropTypes.string.isRequired
+  }).isRequired
+};
+
+export default BookCard;
